8e2c0a996903abdc9180c3dc0f035a8a757a0659
Alejandro Dietta <alexdietta@hotmail.com>
2017-11-30 09:46:40 +0100

    Iteration 3 done

--- starter-code/src/app/my-home-component/my-home-component.component.ts
+++ starter-code/src/app/my-home-component/my-home-component.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { CinemaService } from '../services/cinema.service'
+import { IMovie } from '../interfaces/MovieInterface';
 
 @Component({
   selector: 'app-my-home-component',
   templateUrl: './my-home-component.component.html',
-  styleUrls: ['./my-home-component.component.css']
+  styleUrls: ['./my-home-component.component.css'],
+  providers: [ CinemaService ]
 })
 export class MyHomeComponentComponent implements OnInit {
 
-  constructor() { }
+  movies:Array<IMovie>
+  constructor(public service:CinemaService) { }
 
   ngOnInit() {
+    this.movies = (this.service.getMovies());
+    console.log('WTF')
+    console.log('GG',this.movies)
   }
+
+
 
 }
