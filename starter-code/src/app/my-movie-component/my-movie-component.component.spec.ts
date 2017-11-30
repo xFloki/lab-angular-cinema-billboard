84e123c7ac21f30a26530902334cf5c77075f001
Alejandro Dietta <alexdietta@hotmail.com>
2017-11-30 10:02:23 +0100

    Iteration 4 done

--- starter-code/src/app/my-movie-component/my-movie-component.component.spec.ts
+++ starter-code/src/app/my-movie-component/my-movie-component.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ActivatedRoute } from '@angular/router';
 import { MyMovieComponentComponent } from './my-movie-component.component';
 
 describe('MyMovieComponentComponent', () => {
--- starter-code/src/app/my-movie-component/my-movie-component.component.ts
+++ starter-code/src/app/my-movie-component/my-movie-component.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CinemaService } from '../services/cinema.service';
+import { IMovie } from '../interfaces/MovieInterface';
 
 @Component({
   selector: 'app-my-movie-component',
   templateUrl: './my-movie-component.component.html',
-  styleUrls: ['./my-movie-component.component.css']
+  styleUrls: ['./my-movie-component.component.css'],
+  providers: [ CinemaService ]
 })
 export class MyMovieComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(public service:CinemaService, private route: ActivatedRoute) { }
+
+  movieId:number;
+  movie:IMovie;
+
 
   ngOnInit() {
+    this.route.params
+      .subscribe((params) => this.movieId = Number(params['id']));
+    this.movie = this.service.getMovie(this.movieId);
   }
+
+
 
 }
